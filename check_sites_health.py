bca2bb7cf88b1b7c3c5e4fdbcd3e4d0e6c793eec
Evgeny Evseev <pelid80@gmail.com>
2016-10-03 00:43:56 +0300

    adds wireframe for code

---
+++ check_sites_health.py
@@ -0,0 +1,11 @@
+def load_urls4check(path):
+    pass
+
+def is_server_respond_with_200(url):
+    pass
+
+def get_domain_expiration_date(domain_name):
+    pass
+
+if __name__ == '__main__':
+    pass
